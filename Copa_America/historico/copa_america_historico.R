0d824513eb5c85f9f689e0b9a63212581e2fe8f7
spiousas <ignaciospiousas@gmail.com>
2021-06-01 15:04:01 -0300

    Copa America hostorico

---
+++ Copa_America/historico/copa_america_historico.R
@@ -0,0 +1,116 @@
+library(rvest)
+library(tidyverse)
+library(ggbump)
+library(ggflags)
+library(countrycode)
+library(janitor)
+library(paletteer)
+library(ggtext)
+library(extrafonts)
+library(colorspace)
+
+wiki <- "https://en.wikipedia.org/wiki/Copa_América"
+
+rank_coding <- c("winners" = 1, "runners_up" = 2, "third_place" = 3, "fourth_place" = 4)
+
+data_CSera = wiki %>%
+  read_html() %>%
+  html_node(xpath = '//*[@id="mw-content-text"]/div[1]/table[5]') %>%
+  html_table(fill = TRUE) %>%
+  clean_names() %>%
+  mutate(tournament = "Campeonato Sudamericano")
+
+data_CAera = wiki %>%
+  read_html() %>%
+  html_node(xpath = '//*[@id="mw-content-text"]/div[1]/table[6]') %>%
+  html_table(fill = TRUE) %>%
+  clean_names() %>%
+  mutate(tournament = "Copa América")
+
+data_total <- rbind(data_CSera, data_CAera) %>%
+  select(!starts_with("x") & !starts_with("score")) %>%
+  pivot_longer(
+    cols = c("winners", "runners_up", "third_place", "fourth_place"),
+    names_to = "rank",
+    values_to = "country",
+    values_drop_na = TRUE) %>%
+  filter(year<2020) %>%
+  mutate(year = parse_number(year),
+         rank = as.numeric(recode(rank, !!!rank_coding)),
+         is_host = if_else(country==hosts, 1, 0),
+         country_id = countrycode(country,
+                                  origin = "country.name",
+                                  destination = "genc2c") %>% tolower())
+  
+pal <- paletteer_dynamic("cartography::pastel.pal", length(unique(data_total$country)))
+col <- "#3C537A"
+medals <- c("gold", "#C0C0C0", "#cd7f32")
+
+Tournament_id <- c("Copa América")
+
+lines <- tibble(year = rep(data_total %>% filter(tournament %in% Tournament_id) %>% pull(year) %>% unique(), 2),
+                rank = c(rep(1, length(data_total %>% filter(tournament %in% Tournament_id) %>% pull(year) %>% unique())),
+                      rep(4, length(data_total %>% filter(tournament %in% Tournament_id) %>% pull(year) %>% unique()))))
+
+fig.CAm <- data_total %>% filter(tournament %in% Tournament_id) %>%
+  ggplot(aes(x = year, 
+             y = rank)) +
+  annotate("rect", 
+           xmin = 1970, 
+           xmax = 2020, 
+           ymin = c(0.6,1.6, 2.6), 
+           ymax = c(1.4, 2.4, 3.4), 
+           alpha = 0.4, 
+           color = medals, 
+           fill = medals) +
+  annotate("text", 
+           x = 1974, 
+           y = 1:4, 
+           label = c("Campeón", "Subcampeón", "Tercero", "Cuarto"),
+           color = c(darken(medals, amount = 0.7), col),
+           hjust = 1) +
+  geom_line(data = lines, aes(group = year),
+            color = lighten(col,
+                            amount = 0.6),
+            size = 1,
+            linetype = "dotted") +
+  geom_bump(aes(group = country, 
+                color = country),
+            smooth =10, 
+            size = 1.5, 
+            lineend = "round") +
+  geom_flag(aes(country = country_id),
+            size = 8) +
+  geom_label(data = data_total %>% filter(tournament %in% Tournament_id & is_host==1),
+             label = "Anfitrión",
+             family = "JetBrains Mono",
+             size = 3, 
+             nudge_y = 0.27,
+             color = "black",
+             fill = "gray80",
+             fontface = "bold") +
+  coord_cartesian(expand = FALSE,
+                  clip = 'off') +
+  scale_y_reverse(limits = c(4.2, 0.6)) +
+  scale_x_continuous(breaks = data_total %>% filter(tournament %in% Tournament_id) %>% pull(year) %>% unique()) +
+  theme_void(base_family = "JetBrains Mono") +
+  scale_color_manual(values = pal) +
+  labs(title = "Cuatro primeros puestos de la **Copa América** a lo largo de los años<br>",
+       caption = "<br/>**Source:** Wikipedia | **Viz:** @spiousas") +
+  theme(legend.position = "none",
+        plot.margin = unit(c(.2,.2,.2,.2), "cm"),
+        plot.title = element_markdown(color = col),
+        plot.title.position = "panel",
+        plot.subtitle = element_markdown(color = col),
+        plot.caption = element_markdown(color = col),
+        plot.caption.position = "plot",
+        axis.text.x = element_markdown(size = 8, 
+                                       vjust = 0,
+                                       color = col),
+        panel.background = element_rect(fill = lighten(col, amount = .9), 
+                                        colour = NA),
+        plot.background = element_rect(fill = lighten(col, amount = .9), 
+                                colour = NA))
+fig.CAm
+
+ggsave("./Covid_vaccines/output/vaccine_GDP.png", dpi = 300, width = 22, height = 18, units = "cm")
